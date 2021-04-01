bde0bad5c944364386a28f58c9b7ff663952569a
gabriellebraz <gabriellebrazsilva@gmail.com>
2021-04-01 12:27:45 -0300

    codigo organizado

--- estudoJson.js
+++ estudoJson.js
@@ -4,8 +4,6 @@ let pets = bancoDados.pets;
 
 
 const atenderCliente = (pet, servico) => {
-    
-    console.log(`Bem vindo, ${pet.tutor}`);
     console.log(`Olá, ${pet.nome}`);
 
     // if(servico){
@@ -13,7 +11,7 @@ const atenderCliente = (pet, servico) => {
     // }
     (servico) ? servico() : console.log('só vim dar uma olhadinha');
 
-    console.log('Tchau, volte sempre!!');
+    console.log('Tchau, até mais!');
 }
 
 const darBanho = () => {
@@ -24,8 +22,35 @@ const apararUnhas = () => {
     console.log('Aparando unhas...');
 }
 
-atenderCliente(pets[0], darBanho);
-console.log("---------")
-atenderCliente(pets[2], apararUnhas);
-console.log("---------")
-atenderCliente(pets[1]);
+// atenderCliente(pets[0], darBanho);
+// console.log("---------")
+// atenderCliente(pets[2], apararUnhas);
+// console.log("---------")
+// atenderCliente(pets[1]);
+
+
+//desestruturação
+
+// let pessoa = {
+//     nome: 'Iago',
+//     idade: 25,
+//     profissao: 'dev',
+//     contato: '119999999',
+//     habilidades: ['node.js', 'mysql', 'javascript', 'html']
+// }
+
+// let {nome, contato} = pessoa;
+
+// console.log(`${pessoa.nome} - ${pessoa.contato}`);
+// console.log(`${nome} - ${contato}`);
+
+let turma1 = ['Anna', 'João', 'Vinicius', 'Igor'];
+let turma2 = ['Janaina', 'Gustavo', 'Adriana', 'Isaac', 'Anna'];
+
+// let turmasAvanade = [turma1, turma2];
+// let turmasAvanade = [...turma1, ...turma2];
+
+// console.log(turmasAvanade);
+
+// turma1.push(turma2);
+// console.log(turma1)
